c4ccdaf0322ee031e92d0531339b0c9a18a288a4
PeterTrotter <peter.trotter@estrellasystems.com>
2023-06-01 10:00:25 +0100

    improve: be more tolerant with `readUint32` for missing leading byte required by ASN.1 BER

--- test/varbinds.js
+++ test/varbinds.js
@@ -1,22 +1,20 @@
-/* eslint-disable no-unused-expressions */
-
-var ber    = require ('asn1-ber').Ber;
-var assert = require('assert');
-var snmp   = require('../');
+const ber    = require ('asn1-ber').Ber;
+const assert = require('assert');
+const snmp   = require('../');
 
 describe('parseInt()', function() {
 	describe('given a negative integer', function() {
-		var writer = new ber.Writer();
+		const writer = new ber.Writer();
 		writer.writeInt(-3);
-		var reader = new ber.Reader(writer.buffer);
+		const reader = new ber.Reader(writer.buffer);
 		it('returns a negative integer', function() {
 			assert.equal(-3, snmp.ObjectParser.readInt32(reader));
 		});
-	}),
+	});
 	describe('given a positive integer', function() {
-		var writer = new ber.Writer();
+		const writer = new ber.Writer();
 		writer.writeInt(3245689);
-		var reader = new ber.Reader(writer.buffer);
+		const reader = new ber.Reader(writer.buffer);
 		it('returns a positive integer', function() {
 			assert.equal(3245689, snmp.ObjectParser.readInt32(reader));
 		});
@@ -25,27 +23,27 @@ describe('parseInt()', function() {
 
 describe('parseUint()', function() {
 	describe('given a positive integer', function() {
-		var writer = new ber.Writer();
+		const writer = new ber.Writer();
 		writer.writeInt(3242425);
-		var reader = new ber.Reader(writer.buffer);
+		const reader = new ber.Reader(writer.buffer);
 		it('returns a positive integer', function() {
 			assert.equal(3242425, snmp.ObjectParser.readUint32(reader));
 		});
 	});
 	describe('given a negative integer', function() {
-		var writer = new ber.Writer();
+		const writer = new ber.Writer();
 		writer.writeInt(-3);
-		var reader = new ber.Reader(writer.buffer);
+		const reader = new ber.Reader(writer.buffer);
 		it('returns a positive integer', function() {
 			assert.equal(4294967293, snmp.ObjectParser.readUint32(reader));
 		});
 	});
 	describe('given a large integer', function() {
-		var writer = new ber.Writer();
-		writer.writeInt(4294967293);
-		var reader = new ber.Reader(writer.buffer);
+		const writer = new ber.Writer();
+		writer.writeInt(4294967294);
+		const reader = new ber.Reader(writer.buffer);
 		it('returns a positive integer', function() {
-			assert.equal(4294967293, snmp.ObjectParser.readUint32(reader));
+			assert.equal(4294967294, snmp.ObjectParser.readUint32(reader));
 		});
 	});
 });
